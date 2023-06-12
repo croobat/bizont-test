05d634136e869d82aad4de27599ff6291891a906
Tony Ramírez <tonyramirez.business@outlook.com>
2023-06-12 17:45:49 -0600

    feat(styles): responsive cards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,47 +21,64 @@ const CardGrid: React.FC = () => {
 
   return (
     <div>
+      <h1>
+        Tony's Bizont test
+      </h1>
       <div className="card-grid">
         <div className="card">
-          <h2>Uppercase String</h2>
-          <form onSubmit={handleStringSubmit} className="card-content">
-            <div className="card-inputs">
-              <input
-                type="text"
-                value={inputString}
-                onChange={(e) => setInputString(e.target.value)}
-              />
-              <button type="submit">Submit</button>
+          <div className="card-title">
+            <h2>Uppercase String</h2>
+          </div>
+          <div className="card-content">
+            <div className="card-description">
+              <p>LLorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.orem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
             </div>
-            {uppercasedString && (
-              <div className="card-output">
-                <p>{uppercasedString}</p>
+            <form onSubmit={handleStringSubmit} className="card-form">
+              <div className="card-inputs">
+                <input
+                  type="text"
+                  value={inputString}
+                  onChange={(e) => setInputString(e.target.value)}
+                />
+                <button type="submit">Submit</button>
               </div>
-            )}
-          </form>
+              {uppercasedString && (
+                <div className="card-output">
+                  <p>{uppercasedString}</p>
+                </div>
+              )}
+            </form>
+          </div>
         </div>
         <div className="card">
-          <h2>Sum Numbers</h2>
-          <form onSubmit={handleSumSubmit} className="card-content">
-            <div className="card-inputs">
-              <input
-                type="number"
-                value={num1}
-                onChange={(e) => setNum1(Number(e.target.value))}
-              />
-              <input
-                type="number"
-                value={num2}
-                onChange={(e) => setNum2(Number(e.target.value))}
-              />
-              <button type="submit">Submit</button>
+          <div className="card-title">
+            <h2>Sum Numbers</h2>
+          </div>
+          <div className="card-content">
+            <div className="card-description">
+              <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</p>
             </div>
-            {sum && (
-              <div className="card-output">
-                <p>{sum}</p>
+            <form onSubmit={handleSumSubmit} className="card-form">
+              <div className="card-inputs">
+                <input
+                  type="number"
+                  value={num1}
+                  onChange={(e) => setNum1(Number(e.target.value))}
+                />
+                <input
+                  type="number"
+                  value={num2}
+                  onChange={(e) => setNum2(Number(e.target.value))}
+                />
+                <button type="submit">Submit</button>
               </div>
-            )}
-          </form>
+              {sum && (
+                <div className="card-output">
+                  <p>{sum}</p>
+                </div>
+              )}
+            </form>
+          </div>
         </div>
       </div>
     </div>
