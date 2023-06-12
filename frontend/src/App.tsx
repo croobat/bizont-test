4b5411ddbcfabbdf0a4f5816a33e02401b115aad
Tony Ramírez <tonyramirez.business@outlook.com>
2023-06-12 16:57:34 -0600

    feat(styles): add card styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,22 @@
 import React, { useState } from "react";
-import axios from "axios";
 import './normalize.css';
 import './App.css';
 
-const App: React.FC = () => {
+const CardGrid: React.FC = () => {
   const [inputString, setInputString] = useState("");
   const [uppercasedString, setUppercasedString] = useState("");
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   const [sum, setSum] = useState(0);
 
-  const handleStringSubmit = async (e: React.FormEvent) => {
+  const handleStringSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    try {
-      const response = await axios.post("/api/uppercase", { inputString });
-      setUppercasedString(response.data);
-    } catch (error) {
-      console.log(error);
-    }
+    setUppercasedString(inputString.toUpperCase());
   };
 
-  const handleSumSubmit = async (e: React.FormEvent) => {
+  const handleSumSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    try {
-      const response = await axios.post("/api/sum", { num1, num2 });
-      setSum(response.data);
-    } catch (error) {
-      console.log(error);
-    }
+    setSum(num1 + num2);
   };
 
   return (
@@ -35,36 +24,48 @@ const App: React.FC = () => {
       <div className="card-grid">
         <div className="card">
           <h2>Uppercase String</h2>
-          <form onSubmit={handleStringSubmit}>
-            <input
-              type="text"
-              value={inputString}
-              onChange={(e) => setInputString(e.target.value)}
-            />
-            <button type="submit">Submit</button>
+          <form onSubmit={handleStringSubmit} className="card-content">
+            <div className="card-inputs">
+              <input
+                type="text"
+                value={inputString}
+                onChange={(e) => setInputString(e.target.value)}
+              />
+              <button type="submit">Submit</button>
+            </div>
+            {uppercasedString && (
+              <div className="card-output">
+                <p>{uppercasedString}</p>
+              </div>
+            )}
           </form>
-          {uppercasedString && <p>{uppercasedString}</p>}
         </div>
         <div className="card">
           <h2>Sum Numbers</h2>
-          <form onSubmit={handleSumSubmit}>
-            <input
-              type="number"
-              value={num1}
-              onChange={(e) => setNum1(Number(e.target.value))}
-            />
-            <input
-              type="number"
-              value={num2}
-              onChange={(e) => setNum2(Number(e.target.value))}
-            />
-            <button type="submit">Submit</button>
+          <form onSubmit={handleSumSubmit} className="card-content">
+            <div className="card-inputs">
+              <input
+                type="number"
+                value={num1}
+                onChange={(e) => setNum1(Number(e.target.value))}
+              />
+              <input
+                type="number"
+                value={num2}
+                onChange={(e) => setNum2(Number(e.target.value))}
+              />
+              <button type="submit">Submit</button>
+            </div>
+            {sum && (
+              <div className="card-output">
+                <p>{sum}</p>
+              </div>
+            )}
           </form>
-          {sum && <p>{sum}</p>}
         </div>
       </div>
     </div>
   );
 };
 
-export default App;
+export default CardGrid;
